1c799062314591da7988826910612036c2c17473
WHAT <ygyshka0@gmail.com>
2023-06-13 21:56:13 +0700

    change 100% cover tests for work

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,3 +11,9 @@ class TestArrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+
+    def test_normalized_start(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3], 0, 2), [1, 2])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], -2, 3), [])
+        self.assertEqual(arrs.my_slice([], 0, 3), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -5, 3), [1, 2, 3])
